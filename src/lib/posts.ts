60836592bde46d8c649b016c683d9c143bba6333
Nate Rush <naterush1997@gmail.com>
2024-06-24 16:56:23 -0700

    naterush.io: move public posts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), '_posts');
+const postsDirectory = path.join(process.cwd(), 'public', '_posts');
 
 export type PostData = {
   id: string;
