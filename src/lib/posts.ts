f2009ffe92c9f69a1fbe0e07ef323eca0718cb82
Nate Rush <naterush1997@gmail.com>
2024-06-24 19:49:54 -0700

    naterush.io: add posts folder path

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,7 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), 'public', '_posts');
+// The directory where the posts are stored is public/_posts
+const postsDirectory = path.join(process.cwd(), 'public/_posts');
 
 export type PostData = {
   id: string;
